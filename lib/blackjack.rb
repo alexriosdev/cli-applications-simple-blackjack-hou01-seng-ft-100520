b60b3bae43734f908e784d86e96e23656ba318af
Alejandro Rios <alexrios.dev@gmail.com>
2020-09-19 08:15:40 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -26,7 +26,8 @@ def initial_round
   card_1 = deal_card
   card_2 = deal_card
   card_sum = card_1 + card_2
-  display_card_total(card_1 + card_2)
+  display_card_total(card_sum)
+  card_sum
 end
 
 def hit?
